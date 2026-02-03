26f8285bcd9b7013195bf58c7dd683c3edf5ef7c
ARZ_projects <attiliozolin@gmail.com>
2026-02-03 01:37:23 -0300

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,10 @@
+export enum InvestmentGoal { ... }
+export enum RiskProfile { ... }
 
+export interface UserProfile { ... }
+export type AssetType = "stock" | "fii" | "fixed_income";
+
+export interface StockData { ... }
+export interface GlossaryTerm { ... }
+export interface Transaction { ... }
+export interface Holding { ... }
